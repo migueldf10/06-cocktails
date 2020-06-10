34094c165476eb7162e1b4477c1b75aa169c7a2a
migueldf10 <migueldf10@gmail.com>
2020-06-10 11:55:43 +0200

    basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
 
-      </header>
-    </div>
+      <div className="App">
+        <Switch>
+          <Router path='/:coctail'>
+            <h1> This is a cocktail page</h1>
+          </Router>
+          <Router path='/'>
+            <h1> This is the home page</h1>
+          </Router>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
